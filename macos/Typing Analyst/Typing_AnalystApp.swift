b55fbbc13a5cf611449f2e94e0ef122c95a1bab0
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2024-12-30 01:45:19 +0530

    macos: Initial commit for creation of the macos application Typing Analyst

---
+++ macos/Typing Analyst/Typing_AnalystApp.swift
@@ -0,0 +1,17 @@
+//
+//  Typing_AnalystApp.swift
+//  Typing Analyst
+//
+//  Created by Avikalp Kumar Gupta on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Typing_AnalystApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ macos/Typing AnalystTests/Typing_AnalystTests.swift
@@ -0,0 +1,17 @@
+//
+//  Typing_AnalystTests.swift
+//  Typing AnalystTests
+//
+//  Created by Avikalp Kumar Gupta on 30/12/24.
+//
+
+import Testing
+@testable import Typing_Analyst
+
+struct Typing_AnalystTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
