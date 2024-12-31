eeb5abd2770528768383dae31a971da40c56be63
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2024-12-31 14:46:17 +0530

    macos: Settings: fixed the UI of the preferences view. (Now everything is correctly centered)

--- macos/Typing Analyst/PreferencesView.swift
+++ macos/Typing Analyst/PreferencesView.swift
@@ -9,27 +9,44 @@ import SwiftUI
 
 struct PreferencesView: View {
     @Binding var preferences: AppPreferences
+    private let formWidth: CGFloat = 800
 
     var body: some View {
         Form {
-            LabeledContent("Update Frequency (seconds):") {
-                TextField("Update Frequency", value: $preferences.updateFrequency, format: .number)
-                    .frame(width: 50) // Set a fixed width for the TextField
-            }
-            LabeledContent("Data Time Window (seconds):") {
-                TextField("Data Time Window", value: $preferences.dataTimeWindow, format: .number)
-                    .frame(width: 75)
-            }
-            LabeledContent("Chart Time Window (seconds):") {
-                TextField("Chart Time Window", value: $preferences.chartTimeWindow, format: .number)
-                    .frame(width: 75)
-            }
+            VStack(spacing: 15) {
+                HStack {
+                    Text("Update Frequency (seconds)")
+                    TextField("", value: $preferences.updateFrequency, format: .number)
+                        .multilineTextAlignment(.center)
+                        .frame(width: 100)
+                }
 
-            Toggle("Show WPM Chart", isOn: $preferences.showWPMChart)
-            Toggle("Show CPM Chart", isOn: $preferences.showCPMChart)
-            Toggle("Show Accuracy Chart", isOn: $preferences.showAccuracyChart)
+                HStack {
+                    Text("Data Time Window (seconds)")
+                    TextField("", value: $preferences.dataTimeWindow, format: .number)
+                        .multilineTextAlignment(.center)
+                        .frame(width: 100)
+                }
+
+                HStack {
+                    Text("Chart Time Window (seconds)")
+                    TextField("", value: $preferences.chartTimeWindow, format: .number)
+                        .multilineTextAlignment(.center)
+                        .frame(width: 100)
+                }
+
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Charts to show in popover")
+                        .font(.headline)
+                        .padding(.top)
+
+                    Toggle("Show WPM Chart", isOn: $preferences.showWPMChart)
+                    Toggle("Show CPM Chart", isOn: $preferences.showCPMChart)
+                    Toggle("Show Accuracy Chart", isOn: $preferences.showAccuracyChart)
+                }
+            }
+            .frame(width: formWidth)
+            .padding()
         }
-        .padding()
-        .frame(width: 300) // Keep a fixed width for the entire Form
     }
 }
