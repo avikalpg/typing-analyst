f57222a7a41b818d680083424f8dc8af7be068fd
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2025-01-31 11:18:56 +0530

    web: feat: add link to Typing Stats in Header component

--- web/src/app/components/Header.tsx
+++ web/src/app/components/Header.tsx
@@ -58,9 +58,17 @@ export default function Header() {
 						{isMenuOpen && (
 							<menu className="absolute right-0 mt-2 w-48 py-2 bg-white dark:bg-gray-800 rounded-lg shadow-xl">
 								<li>
+									<Link
+										href='/typing-stats'
+										className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-700"
+									>
+										Typing Stats
+									</Link>
+								</li>
+								<li>
 									<button
 										onClick={handleLogout}
-										className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-700"
+										className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100 dark:hover:bg-gray-700 bg-white dark:bg-gray-800 text-primary"
 									>
 										Logout
 									</button>
